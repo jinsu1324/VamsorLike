b7eab38afaf653b30ee1f42c9b2391dd7a46a1eb
skypoweras <skypoweras@gmail.com>
2024-09-11 01:02:24 +0900

    스킬레벨데이터 에디터윈도우버튼으로 셋팅되게

--- Assets/Scripts/Editor/SkillLevelDataSettingEditorWindow.cs
+++ Assets/Scripts/Editor/SkillLevelDataSettingEditorWindow.cs
@@ -1,3 +1,5 @@
+using Sirenix.OdinInspector;
+using Sirenix.OdinInspector.Editor;
 using System;
 using System.Collections;
 using System.Collections.Generic;
@@ -5,26 +7,41 @@ using System.Reflection;
 using UnityEditor;
 using UnityEngine;
 
-public class Test : MonoBehaviour
+public class SkillLevelDataSettingEditorWindow : OdinEditorWindow
 {
-    public TextAsset TextAsset_SlashAttack;
-    public TextAsset TextAsset_Boomerang;
-    public TextAsset TextAsset_Sniper;
+    // 스킬 레벨 데이터 csv
+    [SerializeField]
+    private TextAsset TextAsset_SlashAttack;
+    [SerializeField]
+    private TextAsset TextAsset_Boomerang;
+    [SerializeField]
+    private TextAsset TextAsset_Sniper;
 
-    public const string FILENAME_SlashAttack = "SkillLevelData_SlashAttack";
-    public const string FILENAME_Boomerang = "SkillLevelData_Boomerang";
-    public const string FILENAME_Sniper = "SkillLevelData_Sniper";
+    // 스킬 레벨 데이터 파일이름
+    private const string FILENAME_SlashAttack = "SkillLevelData_SlashAttack";
+    private const string FILENAME_Boomerang = "SkillLevelData_Boomerang";
+    private const string FILENAME_Sniper = "SkillLevelData_Sniper";
 
-    private void Start()
+
+    // 메뉴 생성
+    [MenuItem("내 메뉴/스킬 레벨 데이터 셋팅")]
+    public static void OpenWindow()
+    {
+        GetWindow<SkillLevelDataSettingEditorWindow>().Show();
+    }
+
+
+    [Button("스킬 레벨 데이터 셋팅", ButtonSizes.Large)]
+    public void SkillLevelDataSettingButton()
     {
         CSV_Save_SkillLevelData<SkillLevelData_SlashAttack, SkillData_SlashAttack>(TextAsset_SlashAttack, FILENAME_SlashAttack);
         CSV_Save_SkillLevelData<SkillLevelData_Boomerang, SkillData_Boomerang>(TextAsset_Boomerang, FILENAME_Boomerang);
         CSV_Save_SkillLevelData<SkillLevelData_Sniper, SkillData_Sniper>(TextAsset_Sniper, FILENAME_Sniper);
     }
 
+    // csv를 SkillLevelData 스크립터블로 저장
     public static void CSV_Save_SkillLevelData<SkillLevelData, SkillData>(TextAsset textAsset, string fileName) where SkillLevelData : SkillLevelDataBase<SkillData> where SkillData : new()
     {
-
         // 스크립터블 가져오는 부분
         string path = $"Assets/Resources/Data/Skill/{fileName}.asset";
         ScriptableObject so = AssetDatabase.LoadAssetAtPath<SkillLevelData>(path);
