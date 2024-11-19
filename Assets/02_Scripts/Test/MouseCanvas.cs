3ab9084100e3d1a177bd34a3a38ce95c0f2c9cb0
skypoweras <skypoweras@gmain.com>
2024-11-19 22:51:49 +0900

    커서 유니티에디터에서만 보이도록

--- Assets/02_Scripts/Test/MouseCanvas.cs
+++ Assets/02_Scripts/Test/MouseCanvas.cs
@@ -4,9 +4,15 @@ public class MouseCanvas : MonoBehaviour
 {
     public RectTransform cursorImage;
 
+    private void Awake()
+    {
+        cursorImage.gameObject.SetActive(false);
+    }
+
 #if UNITY_EDITOR // Unity Editor에서만 실행
     void Start()
     {
+        cursorImage.gameObject.SetActive(true);    
         Cursor.visible = false;
     }
 
